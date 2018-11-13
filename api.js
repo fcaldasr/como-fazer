8f15166284f16b1dea5cfc1348353bd2377f0c92
Filipe <filipejogos11@hotmail.com>
2018-11-13 14:02:10 -0200

    commit fullstack

---
+++ api.js
@@ -0,0 +1,39 @@
+const axios = require('axios')
+const baseURL = 'https://como-fazer-filipe.firebaseio.com/'
+const auth = 'KpyGh7jTjd5iH7Otv2MqLYlMDO9ySBJ4SdGMs5ND'
+
+const list = async(key) => {
+    const content = await axios.get(baseURL + key + '.json?auth='+auth)
+    if(content.data){
+        const objetos = Object.keys(content.data).map(key => {
+            return {
+                id: key,
+                ...content.data[key]
+            }
+        })
+        return objetos
+    }
+    return []
+}
+const apagar = async(key,id) => {
+    await axios.delete(baseURL+ key + '/'+ id+ '.json?auth='+auth)
+    return true
+}
+const get = async(key, id) => {
+    const content = await axios.get(`${baseURL}/${key}/${id}.json?auth=`+auth)
+    return{
+            id: id,
+            ...content.data
+    }
+}
+const update = async(key, id, data) => {
+    await axios.put(`${baseURL}/${key}/${id}.json?auth=`+auth, data)
+    return true
+}
+const create = async (key, data) => {
+    await axios.post(`${baseURL}/${key}.json?auth=`+auth,data)
+    return true
+}
+module.exports = {
+    list, apagar, get, update, create
+}
